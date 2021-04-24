8ac73f0bdea8f0d6df03f8d922af84ee8487ea32
lucky091588 <lucky091588@gmail.com>
2021-04-24 23:47:06 +0800

    Remove civicrm breadcrumbs in the page

--- CRM/Foobar/Page/MyPage.php
+++ CRM/Foobar/Page/MyPage.php
@@ -4,6 +4,7 @@ use CRM_Foobar_ExtensionUtil as E;
 class CRM_Foobar_Page_MyPage extends CRM_Core_Page {
 
   public function run() {
+    CRM_Utils_System::resetBreadCrumb();
     // Example: Set the page-title dynamically; alternatively, declare a static title in xml/Menu/*.xml
     CRM_Utils_System::setTitle(E::ts('MyPage'));
 
